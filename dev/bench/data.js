6eaa6adf99123c6f1b4cce335e418350b99adfdf
github-action-benchmark <github@users.noreply.github.com>
2020-11-18 11:48:13 +0000

    add Benchmark (go) benchmark result for dd681cda878837ba45e1f1756027ad479a61a15e

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1605700093159,
+  "repoUrl": "https://github.com/krystal/go-katapult",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "contact@jimeh.me",
+            "name": "Jim Myhrberg",
+            "username": "jimeh"
+          },
+          "committer": {
+            "email": "contact@jimeh.me",
+            "name": "Jim Myhrberg",
+            "username": "jimeh"
+          },
+          "distinct": true,
+          "id": "dd681cda878837ba45e1f1756027ad479a61a15e",
+          "message": "ci(benchmarks): add benchmark reports to GitHub Actions",
+          "timestamp": "2020-11-18T11:44:17Z",
+          "tree_id": "0ed4a1ccf27da008754dc3dfe0ce70c12f32ec8c",
+          "url": "https://github.com/krystal/go-katapult/commit/dd681cda878837ba45e1f1756027ad479a61a15e"
+        },
+        "date": 1605700092232,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkNameGenerator_RandomHostname",
+            "value": 3559,
+            "unit": "ns/op",
+            "extra": "325344 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
