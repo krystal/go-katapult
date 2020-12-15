ec659abcb1eca36e7a5a47bcd1c1033b8cc79995
Jim Myhrberg <contact@jimeh.me>
2020-12-15 16:52:27 +0000

    docs(buildspec): improve examples by using a buildspec_test package name

--- pkg/buildspec/virtual_machine_spec_example_test.go
+++ pkg/buildspec/virtual_machine_spec_example_test.go
@@ -1,56 +1,64 @@
-package buildspec
+package buildspec_test
 
 import (
 	"fmt"
+
+	"github.com/krystal/go-katapult/pkg/buildspec"
 )
 
 //nolint:lll
 func ExampleVirtualMachineSpec_JSON() {
-	spec := &VirtualMachineSpec{
-		DataCenter: &DataCenter{Permalink: "london"},
-		Resources: &Resources{
-			Package: &Package{Permalink: "rock-3"},
+	spec := &buildspec.VirtualMachineSpec{
+		DataCenter: &buildspec.DataCenter{Permalink: "london"},
+		Resources: &buildspec.Resources{
+			Package: &buildspec.Package{Permalink: "rock-3"},
 		},
-		DiskTemplate: &DiskTemplate{Permalink: "templates/ubuntu-18-04"},
-		Hostname:     "web-3",
+		DiskTemplate: &buildspec.DiskTemplate{
+			Permalink: "templates/ubuntu-18-04",
+		},
+		Hostname: "web-3",
 	}
-	x, _ := spec.JSON()
+	out, _ := spec.JSON()
 
-	fmt.Println(string(x))
+	fmt.Println(string(out))
 	// Output:
 	// {"data_center":{"permalink":"london"},"resources":{"package":{"permalink":"rock-3"}},"disk_template":{"permalink":"templates/ubuntu-18-04"},"hostname":"web-3"}
 }
 
 //nolint:lll
 func ExampleVirtualMachineSpec_XML() {
-	spec := &VirtualMachineSpec{
-		DataCenter: &DataCenter{Permalink: "london"},
-		Resources: &Resources{
-			Package: &Package{Permalink: "rock-3"},
+	spec := &buildspec.VirtualMachineSpec{
+		DataCenter: &buildspec.DataCenter{Permalink: "london"},
+		Resources: &buildspec.Resources{
+			Package: &buildspec.Package{Permalink: "rock-3"},
 		},
-		DiskTemplate: &DiskTemplate{Permalink: "templates/ubuntu-18-04"},
-		Hostname:     "web-3",
+		DiskTemplate: &buildspec.DiskTemplate{
+			Permalink: "templates/ubuntu-18-04",
+		},
+		Hostname: "web-3",
 	}
-	x, _ := spec.XML()
+	out, _ := spec.XML()
 
-	fmt.Println(string(x))
+	fmt.Println(string(out))
 	// Output:
 	// <?xml version="1.0" encoding="UTF-8"?>
 	// <VirtualMachineSpec><DataCenter by="permalink">london</DataCenter><Resources><Package by="permalink">rock-3</Package></Resources><DiskTemplate><DiskTemplate by="permalink">templates/ubuntu-18-04</DiskTemplate></DiskTemplate><Hostname><Hostname>web-3</Hostname></Hostname></VirtualMachineSpec>
 }
 
 func ExampleVirtualMachineSpec_YAML() {
-	spec := &VirtualMachineSpec{
-		DataCenter: &DataCenter{Permalink: "london"},
-		Resources: &Resources{
-			Package: &Package{Permalink: "rock-3"},
+	spec := &buildspec.VirtualMachineSpec{
+		DataCenter: &buildspec.DataCenter{Permalink: "london"},
+		Resources: &buildspec.Resources{
+			Package: &buildspec.Package{Permalink: "rock-3"},
 		},
-		DiskTemplate: &DiskTemplate{Permalink: "templates/ubuntu-18-04"},
-		Hostname:     "web-3",
+		DiskTemplate: &buildspec.DiskTemplate{
+			Permalink: "templates/ubuntu-18-04",
+		},
+		Hostname: "web-3",
 	}
-	x, _ := spec.YAML()
+	out, _ := spec.YAML()
 
-	fmt.Println(string(x))
+	fmt.Println(string(out))
 	// Output:
 	// data_center:
 	//   permalink: london
