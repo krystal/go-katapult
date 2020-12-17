bced21f88d571b18bef95aa52b71b3b20512bff5
Jim Myhrberg <contact@jimeh.me>
2020-12-17 12:17:50 +0000

    feat(buildspec): add IPVersion type to constrain IP versions to 4 or 6

--- pkg/buildspec/ip_address_allocation.go
+++ pkg/buildspec/ip_address_allocation.go
@@ -10,6 +10,6 @@ const (
 type IPAddressAllocation struct {
 	Type      IPAddressAllocationType `xml:"type,attr,omitempty" json:"type,omitempty" yaml:"type,omitempty"`
 	IPAddress *IPAddress              `xml:",omitempty" json:"ip_address,omitempty" yaml:"ip_address,omitempty"`
-	Version   int                     `xml:",omitempty" json:"version,omitempty" yaml:"version,omitempty"`
+	Version   IPVersion               `xml:",omitempty" json:"version,omitempty" yaml:"version,omitempty"`
 	Subnet    *Subnet                 `xml:",omitempty" json:"subnet,omitempty" yaml:"subnet,omitempty"`
 }
---
+++ pkg/buildspec/ip_version.go
@@ -0,0 +1,8 @@
+package buildspec
+
+type IPVersion int
+
+const (
+	IPv4 IPVersion = 4
+	IPv6 IPVersion = 6
+)
