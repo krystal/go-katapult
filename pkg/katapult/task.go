45899cf5023b7ee3be81de5b338e86c53a8005b1
Jim Myhrberg <contact@jimeh.me>
2021-01-05 18:50:05 +0000

    feat(task): add Task resource methods

--- pkg/katapult/task.go
+++ pkg/katapult/task.go
@@ -1,6 +1,12 @@
 package katapult
 
-import "github.com/augurysys/timestamp"
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/augurysys/timestamp"
+)
 
 type TaskStatus string
 
@@ -19,4 +25,51 @@ type Task struct {
 	StartedAt  *timestamp.Timestamp `json:"started_at,omitempty"`
 	FinishedAt *timestamp.Timestamp `json:"finished_at,omitempty"`
 	Progress   int                  `json:"progress,omitempty"`
+}
+
+type tasksResponseBody struct {
+	Task *Task `json:"task,omitempty"`
+}
+
+type TasksClient struct {
+	client   *apiClient
+	basePath *url.URL
+}
+
+func newTasksClient(c *apiClient) *TasksClient {
+	return &TasksClient{
+		client:   c,
+		basePath: &url.URL{Path: "/core/v1/"},
+	}
+}
+
+func (s *TasksClient) Get(
+	ctx context.Context,
+	id string,
+) (*Task, *Response, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("tasks/%s", id),
+	}
+
+	body, resp, err := s.doRequest(ctx, "GET", u, nil)
+
+	return body.Task, resp, err
+}
+
+func (s *TasksClient) doRequest(
+	ctx context.Context,
+	method string,
+	u *url.URL,
+	body interface{},
+) (*tasksResponseBody, *Response, error) {
+	u = s.basePath.ResolveReference(u)
+	respBody := &tasksResponseBody{}
+	resp := &Response{}
+
+	req, err := s.client.NewRequestWithContext(ctx, method, u, body)
+	if err == nil {
+		resp, err = s.client.Do(req, respBody)
+	}
+
+	return respBody, resp, err
 }
