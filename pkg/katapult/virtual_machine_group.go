680721c1cda521bc0f6be0572ff19b215071add6
Andrew Tait <ajtait.uk@gmail.com>
2021-03-04 16:07:19 +0000

    feat(virtual_machine_groups): Get, Create, Update and Delete (#77)

--- pkg/katapult/virtual_machine_group.go
+++ pkg/katapult/virtual_machine_group.go
@@ -2,6 +2,7 @@ package katapult
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 
 	"github.com/augurysys/timestamp"
@@ -14,13 +15,52 @@ type VirtualMachineGroup struct {
 	CreatedAt *timestamp.Timestamp `json:"created_at,omitempty"`
 }
 
-type VirtualMachineGroupsClient struct {
-	client   *apiClient
-	basePath *url.URL
+func (s *VirtualMachineGroup) lookupReference() *VirtualMachineGroup {
+	if s == nil {
+		return nil
+	}
+
+	return &VirtualMachineGroup{ID: s.ID}
+}
+
+func (s *VirtualMachineGroup) queryValues() *url.Values {
+	v := &url.Values{}
+
+	if s != nil {
+		v.Set("virtual_machine_group[id]", s.ID)
+	}
+
+	return v
+}
+
+type VirtualMachineGroupCreateArguments struct {
+	Name      string `json:"name,omitempty"`
+	Segregate *bool  `json:"segregate,omitempty"`
+}
+
+type VirtualMachineGroupUpdateArguments struct {
+	Name      string `json:"name,omitempty"`
+	Segregate *bool  `json:"segregate,omitempty"`
+}
+
+type virtualMachineGroupCreateRequest struct {
+	Organization *Organization                       `json:"organization,omitempty"`
+	Properties   *VirtualMachineGroupCreateArguments `json:"properties,omitempty"`
+}
+
+type virtualMachineGroupUpdateRequest struct {
+	VirtualMachineGroup *VirtualMachineGroup                `json:"virtual_machine_group,omitempty"`
+	Properties          *VirtualMachineGroupUpdateArguments `json:"properties,omitempty"`
 }
 
 type virtualMachineGroupsResponseBody struct {
 	VirtualMachineGroups []*VirtualMachineGroup `json:"virtual_machine_groups,omitempty"`
+	VirtualMachineGroup  *VirtualMachineGroup   `json:"virtual_machine_group,omitempty"`
+}
+
+type VirtualMachineGroupsClient struct {
+	client   *apiClient
+	basePath *url.URL
 }
 
 func newVirtualMachineGroupsClient(
@@ -45,6 +85,70 @@ func (s *VirtualMachineGroupsClient) List(
 	body, resp, err := s.doRequest(ctx, "GET", u, nil)
 
 	return body.VirtualMachineGroups, resp, err
+}
+
+func (s *VirtualMachineGroupsClient) Get(
+	ctx context.Context,
+	id string,
+) (*VirtualMachineGroup, *Response, error) {
+	return s.GetByID(ctx, id)
+}
+
+func (s *VirtualMachineGroupsClient) GetByID(
+	ctx context.Context,
+	id string,
+) (*VirtualMachineGroup, *Response, error) {
+	u := &url.URL{
+		Path: fmt.Sprintf("virtual_machine_groups/%s", id),
+	}
+
+	body, resp, err := s.doRequest(ctx, "GET", u, nil)
+
+	return body.VirtualMachineGroup, resp, err
+}
+
+func (s *VirtualMachineGroupsClient) Create(
+	ctx context.Context,
+	org *Organization,
+	args *VirtualMachineGroupCreateArguments,
+) (*VirtualMachineGroup, *Response, error) {
+	u := &url.URL{Path: "organizations/_/virtual_machine_groups"}
+	reqBody := &virtualMachineGroupCreateRequest{
+		Organization: org.lookupReference(),
+		Properties:   args,
+	}
+
+	body, resp, err := s.doRequest(ctx, "POST", u, reqBody)
+
+	return body.VirtualMachineGroup, resp, err
+}
+
+func (s *VirtualMachineGroupsClient) Update(
+	ctx context.Context,
+	group *VirtualMachineGroup,
+	args *VirtualMachineGroupUpdateArguments,
+) (*VirtualMachineGroup, *Response, error) {
+	u := &url.URL{Path: "virtual_machine_groups/_"}
+	reqBody := &virtualMachineGroupUpdateRequest{
+		VirtualMachineGroup: group.lookupReference(),
+		Properties:          args,
+	}
+
+	body, resp, err := s.doRequest(ctx, "PATCH", u, reqBody)
+
+	return body.VirtualMachineGroup, resp, err
+}
+
+func (s *VirtualMachineGroupsClient) Delete(
+	ctx context.Context,
+	group *VirtualMachineGroup,
+) (*Response, error) {
+	qs := queryValues(group)
+	u := &url.URL{Path: "virtual_machine_groups/_", RawQuery: qs.Encode()}
+
+	_, resp, err := s.doRequest(ctx, "DELETE", u, nil)
+
+	return resp, err
 }
 
 func (s *VirtualMachineGroupsClient) doRequest(
