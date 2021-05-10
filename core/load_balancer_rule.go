1e861a72de78a07b31dd27e138378b26070e607a
Noah Stride <noah@noahstride.co.uk>
2021-05-10 13:52:00 +0100

    refactor: use type alias for enums in lbr

--- core/load_balancer_rule.go
+++ core/load_balancer_rule.go
@@ -7,22 +7,39 @@ import (
 	"net/url"
 )
 
+// TODO: Decide naming on this
+type LoadBalancerRuleAlgorithm string
+
+const (
+	RoundRobinRuleAlgorithm       LoadBalancerRuleAlgorithm = "round_robin"
+	LeastConnectionsRuleAlgorithm LoadBalancerRuleAlgorithm = "least_connections"
+	StickyRuleAlgorithm           LoadBalancerRuleAlgorithm = "sticky"
+)
+
+type Protocol string
+
+const (
+	HTTPSProtocol Protocol = "HTTPS"
+	TCPProtocol   Protocol = "TCP"
+	HTTPProtocol  Protocol = "HTTP"
+)
+
 type LoadBalancerRule struct {
-	ID              string     `json:"id,omitempty"`
-	Algorithm       string     `json:"algorithm,omitempty"` // TODO: replace with constrained type?
-	DestinationPort int        `json:"destination_port,omitempty"`
-	ListenPort      int        `json:"listen_port,omitempty"`
-	Protocol        string     `json:"protocol,omitempty"`     // TODO: replace with type?
-	Certificates    []struct{} `json:"certificates,omitempty"` // TODO: is this the same certificate type as certificate.go
-	BackendSSL      bool       `json:"backend_ssl,omitempty"`
-	PassthroughSSL  bool       `json:"passthrough_ssl,omitempty"`
-	CheckEnabled    bool       `json:"check_enabled,omitempty"`
-	CheckFall       int        `json:"check_fall,omitempty"`
-	CheckInterval   int        `json:"check_interval,omitempty"`
-	CheckPath       string     `json:"check_path,omitempty"`
-	CheckProtocol   string     `json:"check_protocol,omitempty"` // TODO: replace with type?
-	CheckRise       int        `json:"check_rise,omitempty"`
-	CheckTimeout    int        `json:"check_timeout,omitempty"`
+	ID              string                    `json:"id,omitempty"`
+	Algorithm       LoadBalancerRuleAlgorithm `json:"algorithm,omitempty"`
+	DestinationPort int                       `json:"destination_port,omitempty"`
+	ListenPort      int                       `json:"listen_port,omitempty"`
+	Protocol        Protocol                  `json:"protocol,omitempty"`
+	Certificates    []Certificate             `json:"certificates,omitempty"`
+	BackendSSL      bool                      `json:"backend_ssl,omitempty"`
+	PassthroughSSL  bool                      `json:"passthrough_ssl,omitempty"`
+	CheckEnabled    bool                      `json:"check_enabled,omitempty"`
+	CheckFall       int                       `json:"check_fall,omitempty"`
+	CheckInterval   int                       `json:"check_interval,omitempty"`
+	CheckPath       string                    `json:"check_path,omitempty"`
+	CheckProtocol   Protocol                  `json:"check_protocol,omitempty"`
+	CheckRise       int                       `json:"check_rise,omitempty"`
+	CheckTimeout    int                       `json:"check_timeout,omitempty"`
 }
 
 type LoadBalancerRuleCreateArguments struct {
