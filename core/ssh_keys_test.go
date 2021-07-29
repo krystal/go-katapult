1bf924b9acaca121f614b1770f041c8081ca0f86
Jake Gealer <jake@gealer.email>
2021-07-29 12:04:55 +0100

    add all unit tests

--- core/ssh_keys_test.go
+++ core/ssh_keys_test.go
@@ -180,3 +180,186 @@ func TestSSHKeysClient_List(t *testing.T) {
 		})
 	}
 }
+
+func TestSSHKeysClient_Add(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		org  OrganizationRef
+		args AuthSSHKeyProperties
+	}
+	tests := []struct {
+		name    string
+		args    args
+		resp    *katapult.Response
+		respErr error
+		respV   *sshKeysResponseBody
+		want    *AuthSSHKey
+		wantReq *katapult.Request
+		wantErr string
+	}{
+		{
+			name: "success",
+			args: args{
+				ctx: context.Background(),
+				org: OrganizationRef{ID: "org_O648YDMEYeLmqdmn"},
+				args: AuthSSHKeyProperties{
+					Name: "test",
+					Key:  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7MZzjBzFWsc6BCcYE2EpSo8DOjzhDPb/WndW6QE/G0xM7iqdlezcmQnL3Gw9jtAOI4OlNok19v4q8C6ham+1WbX2aGd2labOmKoBVWXIzKyFz9pg2Rs10ZGn+Ly+uJF558rSehSvGJPFmKUagYeBG9c/cwuVMzube0yVbtH2CWRs2dMvwhloH5zOh3NMQj/5uBGYMh9uRQKsGHoG8TET08VSok3W/CFilSH7jSmaQYziUqJjOLE2hb8ziCzfv/0GhbY5MoJJUZqUdOlGkYgDMR/IVOHxxF93QBvp1AkAzh8RBsvJPajgZHFa1lWYJRP7U4TREWuxkpaJrbK3I3AHM74GAfIq76wndoFYJhi5qbNgaJjLUJDPPzl8KOcp0Pb5FPqygHWz/K4n1h5SV/LdD0mB487TxeC1NV4XBQQruM5RgfTXSWBW+8W83U0y5h1RNl/Qo9Efo7Kyc25wCxVT2cWRHr3mxZ98p+JxmFmC1KTdUrM95+B7+Hw9fKYvhKz0= jake@Jakes-MacBook-Pro.local",
+				},
+			},
+			respV: &sshKeysResponseBody{
+				SSHKey: &AuthSSHKey{
+					ID:          "testing-id",
+					Name:        "test",
+					Fingerprint: "SHA256:Ybk7/sbyptVqD87piCCz/XHiEKrdvHND2EMDA1qGqRA jake@Jakes-MacBook-Pro.local",
+				},
+			},
+			want: &AuthSSHKey{
+				ID:          "testing-id",
+				Name:        "test",
+				Fingerprint: "SHA256:Ybk7/sbyptVqD87piCCz/XHiEKrdvHND2EMDA1qGqRA jake@Jakes-MacBook-Pro.local",
+			},
+			wantReq: &katapult.Request{
+				Method: "POST",
+				URL: &url.URL{
+					Path: "/core/v1/organizations/_/ssh_keys",
+					RawQuery: url.Values{
+						"organization[id]": []string{
+							"org_O648YDMEYeLmqdmn",
+						},
+					}.Encode(),
+				},
+				Body: AuthSSHKeyProperties{
+					Name: "test",
+					Key:  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7MZzjBzFWsc6BCcYE2EpSo8DOjzhDPb/WndW6QE/G0xM7iqdlezcmQnL3Gw9jtAOI4OlNok19v4q8C6ham+1WbX2aGd2labOmKoBVWXIzKyFz9pg2Rs10ZGn+Ly+uJF558rSehSvGJPFmKUagYeBG9c/cwuVMzube0yVbtH2CWRs2dMvwhloH5zOh3NMQj/5uBGYMh9uRQKsGHoG8TET08VSok3W/CFilSH7jSmaQYziUqJjOLE2hb8ziCzfv/0GhbY5MoJJUZqUdOlGkYgDMR/IVOHxxF93QBvp1AkAzh8RBsvJPajgZHFa1lWYJRP7U4TREWuxkpaJrbK3I3AHM74GAfIq76wndoFYJhi5qbNgaJjLUJDPPzl8KOcp0Pb5FPqygHWz/K4n1h5SV/LdD0mB487TxeC1NV4XBQQruM5RgfTXSWBW+8W83U0y5h1RNl/Qo9Efo7Kyc25wCxVT2cWRHr3mxZ98p+JxmFmC1KTdUrM95+B7+Hw9fKYvhKz0= jake@Jakes-MacBook-Pro.local",
+				},
+			},
+		},
+		{
+			name: "request error",
+			args: args{
+				ctx: context.Background(),
+				org: OrganizationRef{ID: "org_O648YDMEYeLmqdmn"},
+				args: AuthSSHKeyProperties{
+					Name: "test",
+					Key:  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7MZzjBzFWsc6BCcYE2EpSo8DOjzhDPb/WndW6QE/G0xM7iqdlezcmQnL3Gw9jtAOI4OlNok19v4q8C6ham+1WbX2aGd2labOmKoBVWXIzKyFz9pg2Rs10ZGn+Ly+uJF558rSehSvGJPFmKUagYeBG9c/cwuVMzube0yVbtH2CWRs2dMvwhloH5zOh3NMQj/5uBGYMh9uRQKsGHoG8TET08VSok3W/CFilSH7jSmaQYziUqJjOLE2hb8ziCzfv/0GhbY5MoJJUZqUdOlGkYgDMR/IVOHxxF93QBvp1AkAzh8RBsvJPajgZHFa1lWYJRP7U4TREWuxkpaJrbK3I3AHM74GAfIq76wndoFYJhi5qbNgaJjLUJDPPzl8KOcp0Pb5FPqygHWz/K4n1h5SV/LdD0mB487TxeC1NV4XBQQruM5RgfTXSWBW+8W83U0y5h1RNl/Qo9Efo7Kyc25wCxVT2cWRHr3mxZ98p+JxmFmC1KTdUrM95+B7+Hw9fKYvhKz0= jake@Jakes-MacBook-Pro.local",
+				},
+			},
+			respErr: fmt.Errorf("flux capacitor undercharged"),
+			wantErr: "flux capacitor undercharged",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tc := testclient.New(tt.resp, tt.respErr, tt.respV)
+			c := NewSSHKeysClient(tc)
+			ctx := test.Context(tt.args.ctx)
+
+			got, resp, err := c.Add(ctx, tt.args.org, tt.args.args)
+
+			assert.Equal(t, 1, len(tc.Calls), "only 1 request should be made")
+			test.AssertContext(t, ctx, tc.Ctx)
+
+			assert.Equal(t, tt.want, got)
+
+			if tt.resp != nil {
+				assert.Equal(t, tt.resp, resp)
+			}
+
+			if tt.wantReq != nil {
+				assert.Equal(t, tt.wantReq, tc.Request)
+			}
+
+			if tt.wantErr == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestSSHKeysClient_Delete(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		ref  SSHKeyRef
+	}
+	tests := []struct {
+		name    string
+		args    args
+		resp    *katapult.Response
+		respErr error
+		respV   *sshKeysResponseBody
+		want    *AuthSSHKey
+		wantReq *katapult.Request
+		wantErr string
+	}{
+		{
+			name: "success",
+			args: args{
+				ctx: context.Background(),
+				ref: SSHKeyRef{ID: "ssh_O574YEEEYeLmqdmn"},
+			},
+			respV: &sshKeysResponseBody{
+				SSHKey: &AuthSSHKey{
+					ID:          "testing-id",
+					Name:        "test",
+					Fingerprint: "SHA256:Ybk7/sbyptVqD87piCCz/XHiEKrdvHND2EMDA1qGqRA jake@Jakes-MacBook-Pro.local",
+				},
+			},
+			want: &AuthSSHKey{
+				ID:          "testing-id",
+				Name:        "test",
+				Fingerprint: "SHA256:Ybk7/sbyptVqD87piCCz/XHiEKrdvHND2EMDA1qGqRA jake@Jakes-MacBook-Pro.local",
+			},
+			wantReq: &katapult.Request{
+				Method: "DELETE",
+				URL: &url.URL{
+					Path: "/core/v1/ssh_keys/_",
+					RawQuery: url.Values{
+						"ssh_key[id]": []string{
+							"ssh_O574YEEEYeLmqdmn",
+						},
+					}.Encode(),
+				},
+			},
+		},
+		{
+			name: "request error",
+			args: args{
+				ctx: context.Background(),
+				ref: SSHKeyRef{ID: "ssh_O574YEEEYeLmqdmn"},
+			},
+			respErr: fmt.Errorf("flux capacitor undercharged"),
+			wantErr: "flux capacitor undercharged",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tc := testclient.New(tt.resp, tt.respErr, tt.respV)
+			c := NewSSHKeysClient(tc)
+			ctx := test.Context(tt.args.ctx)
+
+			got, resp, err := c.Delete(ctx, tt.args.ref)
+
+			assert.Equal(t, 1, len(tc.Calls), "only 1 request should be made")
+			test.AssertContext(t, ctx, tc.Ctx)
+
+			assert.Equal(t, tt.want, got)
+
+			if tt.resp != nil {
+				assert.Equal(t, tt.resp, resp)
+			}
+
+			if tt.wantReq != nil {
+				assert.Equal(t, tt.wantReq, tc.Request)
+			}
+
+			if tt.wantErr == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, tt.wantErr)
+			}
+		})
+	}
+}
